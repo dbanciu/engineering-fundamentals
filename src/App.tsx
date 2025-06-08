cfea444e74712dfdaa64cf61f599c7f18c6fa43b
benrow4 <benrow@protonmail.com>
2025-06-08 09:37:15 +0200

    feature(react-app): split the App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
-import iptLogo from './assets/ipt.svg'
-import './App.css'
+import "./App.css";
+import Logo from "./Logo";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://ipt.ch" target="_blank">
-          <img src={iptLogo} className="logo ipt" alt="ipt logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Logo />
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
